6aba9984254e1ab21edac89c42ecd66355fa1d7d
Deborah Ferreira <deborah.ferreira@manchester.ac.uk>
2021-01-18 19:05:08 +0000

    fixing flow for BERT.

--- layout_ipa/flows/transformers_based/transformers_train_pair_classification.py
+++ layout_ipa/flows/transformers_based/transformers_train_pair_classification.py
@@ -13,13 +13,13 @@ prepare_rico_task = PrepareRicoScaPair()
 
 layout_lm_model = settings["layout_lm_base"]
 
-# train_path = settings["rico_sca_sample"]["train"]
-# dev_path = settings["rico_sca_sample"]["dev"]
-# test_path = settings["rico_sca_sample"]["test"]
+train_path = settings["rico_sca"]["train"]
+dev_path = settings["rico_sca"]["dev"]
+test_path = settings["rico_sca"]["test"]
 
-train_path = settings["sample_rico_sca"]
-dev_path = settings["sample_rico_sca"]
-test_path = settings["sample_rico_sca"]
+# train_path = settings["sample_rico_sca"]
+# dev_path = settings["sample_rico_sca"]
+# test_path = settings["sample_rico_sca"]
 
 cache_args = dict(
     target="{task_name}-{task_tags}.pkl",
---
+++ layout_ipa/tasks/layout_lm/data_prep/layout_lm_prep_pair.py
@@ -0,0 +1,62 @@
+# Code adapted from https://github.com/microsoft/unilm/blob/master/layoutlm/layoutlm/data/funsd.py
+
+from prefect import Task
+from loguru import logger
+from dynaconf import settings
+import pandas as pd
+from tqdm import tqdm
+from transformers import AutoTokenizer, BertTokenizer, RobertaTokenizer
+from torch.utils.data import Dataset
+import torch
+
+tokenizer_model = settings["layout_lm_base"]
+
+
+class PrepareLayoutLMPairTask(Task):
+    def run(self, input_data, largest=256):
+        logger.info("*** Preprocessing Data for Transformer-Based ***")
+        tokenizer_instruction = BertTokenizer.from_pretrained("bert-base-uncased")
+        entries = dict()
+
+        for id_d, content in tqdm(input_data.items()):
+            ui_element_text = content["ui"]["text"]
+
+            encoded_element = tokenizer_instruction(
+                content["instruction"],
+                ui_element_text,
+                padding="max_length",
+                max_length=largest,
+                truncation=True,
+            )
+
+            entries[id_d] = {
+                "input_ids": encoded_element["input_ids"],
+                "att_mask": encoded_element["attention_mask"],
+                "token_ids": encoded_element["token_type_ids"],
+                "label": content["label"],
+            }
+
+        return TorchDataset(entries)
+
+
+class TorchDataset(Dataset):
+    def __init__(self, dataset):
+        self.dataset = list(dataset.values())
+        self.keys = list(dataset.keys())
+
+    def __getitem__(self, index):
+        instance = self.dataset[index]
+        return (
+            torch.LongTensor(instance["input_ids"]),
+            torch.LongTensor(instance["att_mask"]),
+            torch.LongTensor(instance["token_ids"]),
+            instance["label"],
+            index,
+        )
+
+    def get_id(self, index):
+        return self.keys[index]
+
+    def __len__(self):
+        return len(self.dataset)
+
