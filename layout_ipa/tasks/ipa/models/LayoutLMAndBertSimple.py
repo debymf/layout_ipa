e3322302a347bfc4fdd9f3fef6cb9ccd9541a7ac
Deborah Ferreira <deborah.ferreira@manchester.ac.uk>
2021-02-05 17:00:53 +0000

    adding different model.

--- layout_ipa/tasks/ipa/models/LayoutLMAndBertSimple.py
+++ layout_ipa/tasks/ipa/models/LayoutLMAndBertSimple.py
@@ -88,9 +88,9 @@ class LayoutLMAndBertSimple(PreTrainedModel):
         # self.dropout1 = nn.Dropout(p=0.5)
         # self.dropout2 = nn.Dropout(p=0.5)
         # self.bidaf_layer = BidafAttn(768)
-        self.linear_layer1 = nn.Linear(768 * 1, 1)
+        self.linear_layer1 = nn.Linear(768 * 2, 1)
         # self.linear_layer1 = nn.Linear(768 * 4, 1)
-        self.linear_layer2 = nn.Linear(512, 1)
+        # self.linear_layer2 = nn.Linear(512, 1)
 
         self.act = nn.LogSoftmax(dim=1)
 
@@ -114,21 +114,19 @@ class LayoutLMAndBertSimple(PreTrainedModel):
         # output = output.view(-1, 261)
 
         output_instruction_model = self.model_instruction(**input_instructions)
-        instruction_representation = output_instruction_model[1]
+        output1 = output_instruction_model[1]
         # output1 = self.dropout1(instruction_representation)
         output_ui_model = self.model_ui(**input_ui)
 
-        ui_representation = output_ui_model[1]
+        output2 = output_ui_model[1]
         # output2 = self.dropout2(ui_representation)
         # both_representations = torch.cat(
         #     (ui_representation, instruction_representation), dim=1
         # )
 
         # print(both_representations.shape)
-        # both_representations = torch.cat(
-        #     [output1, output2, torch.abs(output1 - output2), output1 * output2], dim=1
-        # )
-        output = self.linear_layer1(instruction_representation)
+        both_representations = torch.cat([output1, output2], dim=1)
+        output = self.linear_layer1(both_representations)
         # both_representations = self.dropout2(both_representations)
         # output = self.linear_layer2(both_representations)
 
