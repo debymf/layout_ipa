7445a424828fec6ad24809182d93a5a6dd893d6c
Deborah Ferreira <deborah.ferreira@manchester.ac.uk>
2021-02-08 19:59:31 +0000

    adding different model.

--- layout_ipa/tasks/ipa/models/LayoutLMAndBertSimple.py
+++ layout_ipa/tasks/ipa/models/LayoutLMAndBertSimple.py
@@ -89,7 +89,8 @@ class LayoutLMAndBertSimple(PreTrainedModel):
         # self.dropout2 = nn.Dropout(p=0.5)
         # self.bidaf_layer = BidafAttn(768)
         self.linear_layer_instruction = nn.Linear(768, 128)
-        self.linear_layer1 = nn.Linear(128 * 1, 1)
+        self.linear_layer_ui = nn.Linear(768, 128)
+        self.linear_layer_output = nn.Linear(128 * 2, 1)
         # self.linear_layer1 = nn.Linear(768 * 4, 1)
         # self.linear_layer2 = nn.Linear(512, 1)
 
@@ -115,22 +116,21 @@ class LayoutLMAndBertSimple(PreTrainedModel):
         output_instruction_model = self.model_instruction(**input_instructions)
         instruction_embedding = output_instruction_model[1]
         instruction_embedding = self.linear_layer_instruction(instruction_embedding)
-        output1 = F.relu(instruction_embedding)
+        instruction_embedding = F.relu(instruction_embedding)
         # output1 = self.dropout1(instruction_representation)
-        # output_ui_model = self.model_ui(**input_ui)
-
-        # output2 = output_ui_model[1]
+        output_ui_model = self.model_ui(**input_ui)
+        ui_embedding = output_ui_model[1]
+        ui_embedding = self.linear_layer_ui(ui_embedding)
+        ui_embedding = F.relu(ui_embedding)
         # # output2 = self.dropout2(ui_representation)
-        # # both_representations = torch.cat(
-        # #     (ui_representation, instruction_representation), dim=1
-        # # )
+        both_representations = torch.cat((ui_embedding, instruction_embedding), dim=1)
 
         # # print(both_representations.shape)
         # both_representations = torch.cat(
         #     [output1, output2, torch.abs(output1 - output2), output1 * output2], dim=1
         # )
 
-        output = self.linear_layer1(output1)
+        output = self.linear_layer_output(both_representations)
 
         # both_representations = self.dropout2(both_representations)
         # output = self.linear_layer2(both_representations)
