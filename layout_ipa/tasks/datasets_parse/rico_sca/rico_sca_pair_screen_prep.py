7d2d3d4a6f88245b1e5620b791c797eaf001dd04
Deborah Ferreira <deborah.ferreira@manchester.ac.uk>
2021-02-09 21:53:08 +0000

    adding different model.

--- layout_ipa/tasks/datasets_parse/rico_sca/rico_sca_pair_screen_prep.py
+++ layout_ipa/tasks/datasets_parse/rico_sca/rico_sca_pair_screen_prep.py
@@ -126,7 +126,7 @@ class PrepareRicoScaScreenPair(Task):
                             "instruction": instruction,
                             "ui": ui_element,
                             "label": label_ui,
-                            "closest": closest(get_closest(ui_element)),
+                            "closest": self.get_closest(ui_element),
                         }
                         total_pairs = total_pairs + 1
 
