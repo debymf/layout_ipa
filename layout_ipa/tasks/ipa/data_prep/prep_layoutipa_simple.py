36442f67d80462466c4e4e08f469e1f67af31f94
Deborah Ferreira <deborah.ferreira@manchester.ac.uk>
2021-02-09 10:43:08 +0000

    adding different model.

--- layout_ipa/tasks/ipa/data_prep/prep_layoutipa_simple.py
+++ layout_ipa/tasks/ipa/data_prep/prep_layoutipa_simple.py
@@ -17,8 +17,8 @@ class PrepareLayoutIpaSimple(Task):
         self,
         input_data,
         bert_model="bert-base-uncased",
-        largest=512,
-        largest_instruction=512,
+        largest=256,
+        largest_instruction=128,
     ):
         logger.info("*** Preprocessing Data for Layout IPA (simple) ***")
         tokenizer_layout = AutoTokenizer.from_pretrained(tokenizer_model)
