d9401bf9388ed578d8a1e2c03baf41a206bdc423
Deborah Ferreira <deborah.ferreira@manchester.ac.uk>
2021-02-09 11:02:05 +0000

    adding different model.

--- layout_ipa/tasks/ipa/data_prep/prep_layoutipa_simple.py
+++ layout_ipa/tasks/ipa/data_prep/prep_layoutipa_simple.py
@@ -30,6 +30,7 @@ class PrepareLayoutIpaSimple(Task):
                 content["instruction"], content["ui"], largest, tokenizer_layout,
             )
 
+            print(content.keys())
             encoded_instruction = tokenizer_instruction.encode_plus(
                 content["instruction"],
                 content["ui"]["text"],
