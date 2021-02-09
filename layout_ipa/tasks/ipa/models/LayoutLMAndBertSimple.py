c64acd336dc094931f2cfcb1486a7b3c7ce331da
Deborah Ferreira <deborah.ferreira@manchester.ac.uk>
2021-02-09 20:49:23 +0000

    adding different model.

--- layout_ipa/tasks/ipa/models/LayoutLMAndBertSimple.py
+++ layout_ipa/tasks/ipa/models/LayoutLMAndBertSimple.py
@@ -125,7 +125,7 @@ class LayoutLMAndBertSimple(PreTrainedModel):
         # output1 = self.dropout1(instruction_representation)
         output_ui_model = self.model_ui(**input_ui)
         ui_embedding = output_ui_model[1]
-        ui_embedding = self.linear_layer_ui(ui_embedding)
+        # ui_embedding = self.linear_layer_ui(ui_embedding)
         # ui_embedding = F.relu(ui_embedding)
         # # output2 = self.dropout2(ui_representation)
         # both_representations = ui_embedding * instruction_embedding
