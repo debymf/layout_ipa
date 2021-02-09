2886d70f104db6762d61ea2d4748d55760001578
Deborah Ferreira <deborah.ferreira@manchester.ac.uk>
2021-02-09 21:01:30 +0000

    adding different model.

--- layout_ipa/tasks/ipa/models/LayoutLMAndBertSimple.py
+++ layout_ipa/tasks/ipa/models/LayoutLMAndBertSimple.py
@@ -90,6 +90,8 @@ class LayoutLMAndBertSimple(PreTrainedModel):
         self.linear_layer_instruction = nn.Linear(768, 128)
         self.linear_layer_ui = nn.Linear(768, 128)
         self.linear_layer_output = nn.Linear(128 * 2, 1)
+        self.activation_ui = nn.Tanh()
+        self.activation_instruction = nn.Tanh()
         # self.linear_layer1 = nn.Linear(768 * 4, 1)
         # self.linear_layer2 = nn.Linear(512, 1)
 
@@ -121,11 +123,13 @@ class LayoutLMAndBertSimple(PreTrainedModel):
         # print(instruction_embedding.shape)
         # input()
         instruction_embedding = self.linear_layer_instruction(instruction_embedding)
+        instruction_embedding = self.activation_instruction(instruction_embedding)
         # instruction_embedding = F.relu(instruction_embedding)
         # output1 = self.dropout1(instruction_representation)
         output_ui_model = self.model_ui(**input_ui)
         ui_embedding = output_ui_model[1]
         ui_embedding = self.linear_layer_ui(ui_embedding)
+        ui_embedding = self.activation_ui(ui_embedding)
         # ui_embedding = F.relu(ui_embedding)
         # # output2 = self.dropout2(ui_representation)
         # both_representations = ui_embedding * instruction_embedding
