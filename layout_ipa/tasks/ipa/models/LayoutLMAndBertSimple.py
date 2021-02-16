cc23ef43a565a056470d2941e0904b6ed244e9bf
Deborah Ferreira <deborah.ferreira@manchester.ac.uk>
2021-02-16 19:41:30 +0000

    adding different model.

--- layout_ipa/tasks/ipa/models/LayoutLMAndBertSimple.py
+++ layout_ipa/tasks/ipa/models/LayoutLMAndBertSimple.py
@@ -118,14 +118,15 @@ class LayoutLMAndBertSimple(PreTrainedModel):
 
         output_close_elements = output_close_elements.view(-1, 10, 768)
 
-        output_close_elements = output_close_elements.mean(1)
+        output_close_elements = output_close_elements.sum(1)
 
         # screen_embedding = self.linear_layer_ui(output_close_elements)
-        screen_embedding = self.activation_ui1(output_close_elements)
+        # screen_embedding = self.activation_ui1(output_close_elements)
+        screen_embedding = output_close_elements
 
         output_ui_model = self.model_ui(**input_ui)
         ui_embedding = output_ui_model[1]
-        ui_embedding = self.activation_ui2(ui_embedding)
+        # ui_embedding = self.activation_ui2(ui_embedding)
 
         # ui_embedding = self.linear_layer_ui(ui_embedding)
         # ui_embedding = self.activation_ui(ui_embedding)
