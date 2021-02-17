489c5476b46ba5b754aacb3b66601b0ac5ba2057
Deborah Ferreira <deborah.ferreira@manchester.ac.uk>
2021-02-17 22:59:59 +0000

    adding different model.

--- layout_ipa/tasks/ipa/model_pipeline/trainer_layoutipa_simple.py
+++ layout_ipa/tasks/ipa/model_pipeline/trainer_layoutipa_simple.py
@@ -36,7 +36,7 @@ class LayoutIpaSimpleTrainer(Task):
         self.per_gpu_batch_size = kwargs.get("per_gpu_batch_size", 4)
         self.cuda = kwargs.get("cuda", True)
         self.gradient_accumulation_steps = kwargs.get("gradient_accumulation_steps", 1)
-        self.num_train_epochs = kwargs.get("num_train_epochs", 5)
+        self.num_train_epochs = kwargs.get("num_train_epochs", 30)
         self.learning_rate = kwargs.get("learning_rate", 1e-5)
         self.weight_decay = kwargs.get("weight_decay", 0.0)
         self.adam_epsilon = kwargs.get("adam_epsilon", 1e-8)
