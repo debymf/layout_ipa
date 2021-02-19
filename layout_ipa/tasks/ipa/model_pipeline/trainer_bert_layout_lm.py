78dbd369fb6fd1b981643a41e42aa2b698e4d9df
Deborah Ferreira <deborah.ferreira@manchester.ac.uk>
2021-02-19 15:39:27 +0000

    adding different model.

--- layout_ipa/tasks/ipa/model_pipeline/trainer_bert_layout_lm.py
+++ layout_ipa/tasks/ipa/model_pipeline/trainer_bert_layout_lm.py
@@ -27,14 +27,14 @@ from sklearn.metrics import f1_score
 
 np.set_printoptions(threshold=np.inf)
 
-BERT_MODEL = "bert-base-uncased"
+BERT_MODEL = "distilbert-base-uncased"
 LAYOUT_LM_MODEL = "microsoft/layoutlm-base-uncased"
 
 
 class BertandLayoutLMTrainer(Task):
     def __init__(self, **kwargs):
         super(BertandLayoutLMTrainer, self).__init__(**kwargs)
-        self.per_gpu_batch_size = kwargs.get("per_gpu_batch_size", 64)
+        self.per_gpu_batch_size = kwargs.get("per_gpu_batch_size", 32)
         self.cuda = kwargs.get("cuda", True)
         self.gradient_accumulation_steps = kwargs.get("gradient_accumulation_steps", 1)
         self.num_train_epochs = kwargs.get("num_train_epochs", 5)
