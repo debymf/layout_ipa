f149f3bb4603c37e2ef76c37265234d8bc768f55
Deborah Ferreira <deborah.ferreira@manchester.ac.uk>
2021-03-03 10:14:08 +0000

    adding different model.

--- layout_ipa/flows/layout_lm/layout_lm_train_pair_classification.py
+++ layout_ipa/flows/layout_lm/layout_lm_train_pair_classification.py
@@ -14,8 +14,8 @@ prepare_rico_task = PrepareRicoScaPair()
 
 layout_lm_model = settings["layout_lm_base"]
 
-train_path = settings["rico_sca"]["train"]
-dev_path = settings["rico_sca"]["dev"]
+train_path = settings["rico_sca_sample"]["train"]
+dev_path = settings["rico_sca_sample"]["dev"]
 test_path = settings["rico_sca"]["test"]
 
 # train_path = settings["sample_rico_sca"]
