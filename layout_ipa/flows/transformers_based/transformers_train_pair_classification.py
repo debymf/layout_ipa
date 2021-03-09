94c7bdc18812332fbadac0b0d3f1c6fae8af0b3f
Deborah Ferreira <deborah.ferreira@manchester.ac.uk>
2021-03-09 22:43:38 +0000

    adding different model.

--- layout_ipa/flows/transformers_based/transformers_train_pair_classification.py
+++ layout_ipa/flows/transformers_based/transformers_train_pair_classification.py
@@ -36,7 +36,7 @@ cache_args = dict(
     result=LocalResult(dir=f"./cache/datasets/rico/"),
 )
 
-INSTRUCTION_TYPE = [2]
+INSTRUCTION_TYPE = [1]
 #  where: 0 and 3 - Lexical Matching
 #             1 - Spatial (Relative to screen)
 #             2 - Spatial (Relative to other elements)
